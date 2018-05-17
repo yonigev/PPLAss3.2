b8e0272ab040552f6c225097b1ab797e425eb77c
yonig <yonigev@gmail.com>
2018-05-17 15:00:32 +0300

    tested

--- hw3-part3-tests.ts
+++ hw3-part3-tests.ts
@@ -8,3 +8,34 @@ assert.deepEqual(evalParse4(`
 assert.deepEqual(evalParse4(`
     (L4 (if ((lambda ((x lazy)) (= x 10)) 10) #t #f))`),
         true);
+
+
+
+
+
+assert.deepEqual(evalParse4(`
+(L4 (define loop (lambda (x) (loop x)))
+    ((lambda ((f lazy)) 1) (loop 0)))`),1);
+
+assert.deepEqual(evalParse4(`
+    (L4 (define f 
+        (lambda (a (b lazy))
+          a))
+       
+      (f 1 (/ 1 0)))`),1);
+
+assert.deepEqual(evalParse4(`
+      (L4
+        
+(define loop
+    (lambda (x)
+      (loop x)))
+
+  
+    ((lambda (x)
+      ((lambda ((y lazy))
+        (if (= x 0)
+            1
+            y))
+      (loop 0))
+    )0))`),1);
